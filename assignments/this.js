413a1ceabf41641ef6dbad0282ce4e514e1cbdfc
Jose Montero Jr <josemariodev@gmail.com>
2019-05-08 15:45:13 -0500

    completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,48 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. window binding
+ * 2. implicit binding
+ * 3. new binding
+ * 4. explicit binding
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
-// code example for Window Binding
+// function myName(name) {
+//   console.log(this);
+//   return name;
+// }
+// myName('Jose');
 
 // Principle 2
 
-// code example for Implicit Binding
+const object = {
+  greeting: 'Hi',
+  sayHello: function(name) {
+    console.log(`${this.greeting} my name is ${name}`);
+  }
+};
+object.sayHello('Jose');
 
 // Principle 3
 
-// code example for New Binding
+function baseballPlayer(player) {
+  this.greeting = ' is good at baseball';
+  this.player = player;
+  this.speak = function() {
+    console.log(this.player + this.greeting);
+  };
+}
+
+const anthonyRizzo = new baseballPlayer('Rizzo');
+const krisBrant = new baseballPlayer('KB');
+anthonyRizzo.speak();
+krisBrant.speak();
 
 // Principle 4
 
-// code example for Explicit Binding
+anthonyRizzo.speak.call(krisBrant);
+krisBrant.speak.apply(anthonyRizzo);
